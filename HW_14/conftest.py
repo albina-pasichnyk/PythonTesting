bd3e02ab1ea7f733e84a3c9fc447a0d98d24c618
albina-pasichnyk <albina.pasichnyk@gmail.com>
2023-07-06 18:13:20 +0200

    Fix comments

--- HW_14/conftest.py
+++ HW_14/conftest.py
@@ -2,17 +2,38 @@ import pytest
 
 from HW_14.human import Human
 
+import random
+
 
 @pytest.fixture(scope='session', autouse=True)
 def create_human():
-    human = Human('Sam', 18, 'male')
+    ages = random.randint(1, 99)
+    human = Human('Sam', ages, 'male')
     yield human
 
 
 @pytest.fixture()
-def create_diff_human():
+def create_custom_human():
     def __create_human(name, age, gender):
         human = Human(name=name, age=age, gender=gender)
+        return human
+
+    return __create_human
+
+
+@pytest.fixture()
+def create_human_with_age():
+    def __create_human(age):
+        human = Human(name='Same', age=age, gender='male')
+        return human
+
+    return __create_human
+
+
+@pytest.fixture()
+def create_human_with_gender():
+    def __create_human(gender):
+        human = Human(name='Sam', age=18, gender=gender)
         return human
 
     return __create_human
--- HW_14/test_human.py
+++ HW_14/test_human.py
@@ -2,38 +2,42 @@ import pytest
 
 from HW_14.human import Human
 
+import random
+
 
 def test_name_private(create_human):
     """Test name is private"""
     human = create_human
     with pytest.raises(AttributeError, match=f"'{human.__class__.__name__}' object has no attribute '__name'"):
-        assert 'Sam' == human.__name
+        assert 'Sam' == human.__name, 'Name should be private attribute'
 
 
 def test_status_private(create_human):
     """Test status is private"""
     human = create_human
     with pytest.raises(AttributeError, match=f"'{human.__class__.__name__}' object has no attribute '__status'"):
-        assert 'alive' == human.__status
+        assert 'alive' == human.__status, 'Status should be private attribute'
 
 
 def test_age_limit_private(create_human):
     """Test age_limit is private"""
     human = create_human
     with pytest.raises(AttributeError, match=f"'{human.__class__.__name__}' object has no attribute '__age_limit'"):
-        assert '100' == human.__age_limit
+        assert '100' == human.__age_limit, 'Age_limit should be private attribute'
 
 
-def test_age_property(create_human):
+def test_age_property(create_human_with_age):
     """Test age property"""
-    human = create_human
-    assert 18 == human.age
+    age = random.randint(1, 99)
+    human = create_human_with_age(age=age)
+    assert age == human.age, 'Age property of human is incorrect'
 
 
-def test_gender_property(create_human):
+def test_gender_property(create_human_with_gender):
     """Test gender property"""
-    human = create_human
-    assert 'male' == human.gender
+    gender = 'female'
+    human = create_human_with_gender(gender)
+    assert gender == human.gender, 'Gender property of human is incorrect'
 
 
 def test_human_grow(create_human):
@@ -41,34 +45,35 @@ def test_human_grow(create_human):
     human = create_human
     new_age = human.age + 1
     human.grow()
-    assert new_age == human.age
+    assert new_age == human.age, 'Human grows incorrect'
 
 
-def test_human_is_dead(create_diff_human):
+def test_human_is_dead(create_custom_human):
     """
     1. Test that grow is not possible above age limit
     2. Test private function __is_alive
     """
     name = 'Pam'
-    old_human = create_diff_human(name, 100, 'female')
+    old_human = create_custom_human(name, 100, 'female')
     old_human.grow()
     with pytest.raises(Exception, match=f'{name} is already dead...'):
         old_human.grow()
 
 
-def test_change_valid_gender(create_human):
+def test_change_valid_gender(create_human_with_gender):
     """Test valid change of human's gender"""
-    male_human = create_human
+    current_gender = 'male'
     new_gender = 'female'
-    male_human.change_gender('female')
-    assert new_gender == male_human.gender
+    male_human = create_human_with_gender(gender=current_gender)
+    male_human.change_gender(new_gender)
+    assert new_gender == male_human.gender, 'Gender was changed correctly'
 
 
-def test_change_to_current_gender(create_diff_human):
+def test_change_to_current_gender(create_custom_human):
     """Test not possible change of gender to current one"""
     name = 'Sam'
-    male_human = create_diff_human(name, 100, 'male')
     new_gender = 'male'
+    male_human = create_custom_human(name, 100, new_gender)
     with pytest.raises(Exception, match=f"{name} already has gender '{new_gender}'"):
         male_human.change_gender(new_gender)
 
@@ -81,10 +86,10 @@ def test_change_to_non_existing_gender(create_human):
         human.change_gender(new_gender)
 
 
-def test_change_gender_of_dead(create_diff_human):
+def test_change_gender_of_dead(create_custom_human):
     """Test change gender of dead human"""
     human_name = 'Bonny'
-    dead_human = create_diff_human(human_name, 100, 'female')
+    dead_human = create_custom_human(human_name, 100, 'female')
     dead_human.grow()
     with pytest.raises(Exception, match=f"{human_name} is already dead..."):
         dead_human.change_gender('male')
@@ -100,4 +105,4 @@ def test_is_alive_private(create_human):
 def test_validate_gender_private():
     """Test validate_gender method is private"""
     with pytest.raises(AttributeError, match=f"type object 'Human' has no attribute '__validate_gender'"):
-        assert Human.__validate_gender()
+        Human.__validate_gender()
